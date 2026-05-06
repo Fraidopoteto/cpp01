5cf37caf265a62c0e161fc40a8430222ff48cc6a
Fraidopoteto <jonas.schmunk@live.de>
2026-05-06 18:12:01 +0200

    feat: ex00 prototype

---
+++ ex00/main.cpp
@@ -0,0 +1,36 @@
+#include "Zombie.hpp"
+
+int main()
+{
+	std::string user_input;
+
+	bool		is_running = true;
+
+	while (is_running)
+	{
+		std::cout << "\nEnter HEAP or STACK to create a Zombie or EXIT to quit: ";
+		std::getline(std::cin, user_input);
+
+		if (user_input == "HEAP")
+		{
+			std::cout << "\nEnter a Name: ";
+			std::getline(std::cin, user_input);
+
+			Zombie *heapZombie = newZombie(user_input);
+			heapZombie->announce();
+
+			delete heapZombie;
+		}
+
+		if (user_input == "STACK")
+		{
+			std::cout << "\nEnter a Name: ";
+			std::getline(std::cin, user_input);
+
+			randomChump(user_input);
+		}
+
+		if (user_input == "EXIT")
+			is_running = false;
+	}
+}
